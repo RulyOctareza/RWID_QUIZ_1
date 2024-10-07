da9fe3c50d28d312757bedad9b0fb51fcf4c45f7
ruly octareza <rulyoctareza@gmail.com>
2024-10-07 20:13:02 +0900

    Tugas 2, nomor 1

---
+++ lib/quiz_1.dart
@@ -0,0 +1,13 @@
+int calculate() {
+  return 6 * 7;
+}
+/*
+Inputan harus berupa bilangan bulat positif, jika inputan salah sampaikan pesan kepada user
+Soal 4 :
+Buatlah program Bookshelf (Rak Buku), yang memiliki fitur, Add Book, Show Book, Search Book dan Exit.
+Simpan data buku pada List of Map dengan Tipe Data List<Map<String, String>>
+Data buku minimal harus berisi key title, page, author.
+Penyimpanan data buku cukup menggunakan Map, tidak perlu menggunakan Class dan Object.
+Buatlah minimal salah satu menu (boleh lebih) antara add book, show book atau search book menggunakan asynchronous function
+
+*/
